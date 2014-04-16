1b2b4a55cc83d889c55fa1324f5b8b176960633a
Philip Nunoo <ketay99@gmail.com>
2014-04-16 11:56:55 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LooperI::Application.config.secret_key_base = 'e44f7b5d02e61bd7db424797759c07512d2fbfe9cbd20d0c2e1bc2e266894c4079e5fe2b4d1f8336b6da9d93b0186f526d4df1bef2adec6191a337e5c2819b41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LooperI::Application.config.session_store :cookie_store, key: '_looperI_session'
